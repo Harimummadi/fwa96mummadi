9ac69161e8955be068febd6169002bf1445e8e0b
Harimummadi <mummadiharihara9@gmail.com>
2021-10-27 19:42:41 -0500

    modified users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var number=0
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  number=number+1
+  res.send('The number of times User accessesed is : '+number);
 });
 
 module.exports = router;
